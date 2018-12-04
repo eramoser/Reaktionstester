6b6e51a67b7936dd153ef72cd52cc70aba9f9b82
eramoser <eramoser@htl-steyr.ac.at>
2018-12-04 16:00:43 +0100

    Game Move

--- src/ReactionTest/Messages/GameMove.java
+++ src/ReactionTest/Messages/GameMove.java
@@ -5,8 +5,10 @@ import ReactionTest.ReactionButton;
 
 public class GameMove {
     private ReactionButton[] buttons;
+    private long msBeforeStart;
 
-    public GameMove(ReactionButton[] buttons) {
+    public GameMove(ReactionButton[] buttons, long msBeforeStart) {
         this.buttons = buttons;
+        this.msBeforeStart = msBeforeStart;
     }
 }
