99fd324ee695369aed140cca6d3a30d46dcb9895
eramoser <eramoser@htl-steyr.ac.at>
2018-12-04 16:39:42 +0100

    Game Move

--- src/ReactionTest/Messages.java
+++ src/ReactionTest/Messages.java
@@ -1,5 +1,14 @@
 package ReactionTest;
 
 public class Messages {
-    public static final int MESSAGE_GAME_START = 0;
+    public static final int GAME_START = 0;
+    public static final int FINISHED_MOVE = 1;
+
+    public static String getMessage(int messageType, String info){
+        return messageType + ";" + info;
+    }
+
+    public static String getFinishedMoveMessage(){
+        return getMessage(GAME_START, )
+    }
 }
